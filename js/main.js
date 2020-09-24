f17eea361632140abc471aa0d619e735c2f802b3
zenka4 <70884450+zenka4@users.noreply.github.com>
2020-09-24 09:41:19 +0300

    lklkl

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log()
+const savaitesD = ["pirmadiens", "antradienis", "treciadeinis", "ketv","penkt", "sest", "sek"];
+const savDienSk = savaitesD.length;
+
+for (let i = 0; i<savDienSk; i++ ) {
+    console.log(savaitesD[i]);
+}
